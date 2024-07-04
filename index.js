1bd688bc25dff6f448b8c1efb0630b2dd91fb881
Lorenzo <lorenzodidome03@gmail.com>
2024-07-04 21:37:40 +0200

    first-version of the application

---
+++ index.js
@@ -0,0 +1,130 @@
+const express = require("express")
+const {createServer} = require("http")
+const {Server} = require("socket.io")
+
+
+
+const app = express()
+app.use(express.static("www"))
+
+const httpServer = createServer(app)
+
+
+const server = new Server(httpServer)
+
+
+httpServer.listen(8081)
+
+class Player{
+
+    nickname;
+    socket_id;
+    constructor(nickname,socket_id){
+        this.nickname = nickname;
+        this.socket_id = socket_id;
+    }
+}
+
+class Match{
+
+    player1;
+    player2;
+    mossaPlayer1;
+    mossaPlayer2;
+    winplayer1;
+    winplayer2;
+    constructor(player1,player2){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.winplayer1 = 0;
+        this.winplayer2 = 0;
+    }
+}
+
+
+numero_giocatori_connessi = 0;
+giocatori_connessi = []
+match = null;
+id_player = 0;
+server.on("connection", (socket)=>{
+ 
+    
+
+
+      socket.on("login",(message)=>{
+        player = new Player(message.username,socket.id)
+        numero_giocatori_connessi ++;
+        giocatori_connessi.push(player)
+
+        if((numero_giocatori_connessi % 2) == 0){
+ 
+            this.match = new Match(giocatori_connessi[numero_giocatori_connessi-2],giocatori_connessi[numero_giocatori_connessi-1])
+
+            match_players = [giocatori_connessi[numero_giocatori_connessi-2],giocatori_connessi[numero_giocatori_connessi-1]]
+            server.to(giocatori_connessi[numero_giocatori_connessi-2].socket_id).to(giocatori_connessi[numero_giocatori_connessi-1].socket_id).emit("beginMatch",match_players)
+            console.log("Partita iniziata ", this.match.player1.nickname,this.match.player2.nickname)
+
+        }
+      })
+
+
+      socket.on("action",(message)=>{
+        
+
+        
+        if(message.username == this.match.player1.nickname){
+            this.match.mossaPlayer1 = message.move
+            /* e la mando all'opposto*/
+            server.to(this.match.player2.socket_id).emit("againstMove",{move: message.move})
+        }else if(message.username == this.match.player2.nickname){
+            this.match.mossaPlayer2 = message.move
+             /* e la mando all'opposto*/
+             server.to(this.match.player1.socket_id).emit("againstMove",{move: message.move})
+        }
+
+
+        if((this.match.mossaPlayer1!=null)&&(this.match.mossaPlayer2!=null)){
+            if((this.match.mossaPlayer1 == "SASSO") && (this.match.mossaPlayer2=="CARTA")){
+                this.match.winplayer2++;
+                this.match.mossaPlayer1 = null;
+                this.match.mossaPlayer2 = null;
+            }else  if((this.match.mossaPlayer1 == "SASSO") && (this.match.mossaPlayer2=="FORBICE")){
+                this.match.winplayer1++;
+            this.match.mossaPlayer1 = null;
+                this.match.mossaPlayer2 = null;
+            }else if((this.match.mossaPlayer1 == "CARTA") && (this.match.mossaPlayer2=="FORBICE")){
+                this.match.winplayer2++;
+                this.match.mossaPlayer1 = null;
+                this.match.mossaPlayer2 = null;
+            }else  if((this.match.mossaPlayer1 == "CARTA") && (this.match.mossaPlayer2=="SASSO")){
+                this.match.winplayer1++;
+            this.match.mossaPlayer1 = null;
+                this.match.mossaPlayer2 = null;
+            }else if((this.match.mossaPlayer1 == "FORBICE") && (this.match.mossaPlayer2=="CARTA")){
+                this.match.winplayer1++;
+                this.match.mossaPlayer1 = null;
+                this.match.mossaPlayer2 = null;
+            }else if((this.match.mossaPlayer1 == "FORBICE") && (this.match.mossaPlayer2=="SASSO")){
+                this.match.winplayer2++;
+                this.match.mossaPlayer1 = null;
+                this.match.mossaPlayer2 = null;
+            }else{
+                this.match.mossaPlayer1 = null;
+                this.match.mossaPlayer2 = null;
+            }
+        }
+
+
+        if((this.match.winplayer1 >= (this.match.winplayer2+2)) && (this.match.winplayer1>2)){
+            server.to(this.match.player1.socket_id).emit("win_partita")
+            server.to(this.match.player2.socket_id).emit("lose_partita")
+        }else if((this.match.winplayer2 >= (this.match.winplayer1+2)) && (this.match.winplayer2>2)){
+            server.to(this.match.player2.socket_id).emit("win_partita")
+            server.to(this.match.player1.socket_id).emit("lose_partita")
+        }
+        
+        
+
+
+      })
+})
---
+++ www/main.js
@@ -0,0 +1,272 @@
+
+const socket = io();
+
+loginBtn = document.getElementById("loginBtn")
+randomBtn = document.getElementById("randomBtn")
+
+usernameText = document.getElementById("username_text")
+loginDiv = document.getElementById("signin")
+gameDiv = document.getElementById("gamediv")
+player1header = document.getElementById("player1header")
+player2header = document.getElementById("player2header")
+
+
+
+player1container = document.getElementById("player1")
+player2container = document.getElementById("player2")
+
+player2move = document.getElementById("player2move")
+player2btn = document.getElementById("player2btn")
+
+
+player1move = document.getElementById("player1move")
+player1btn = document.getElementById("player1btn")
+
+my_moves = document.getElementsByClassName("my_moves")
+against_moves = document.getElementsByClassName("against_moves")
+
+miamossa = null;
+mossacontro = null;
+currentdiv = null;
+
+wp1 = document.getElementById("wp1")
+wp2 = document.getElementById("wp2")
+lp1 = document.getElementById("losep1")
+lp2 = document.getElementById("losep2")
+vittorePlayer1 = 0;
+vittorePlayer2 = 0;
+sconfittePlayer1 = 0;
+sconfittePlayer2 = 0;
+
+this.loginBtn.addEventListener("click",()=>{
+
+    loginDiv.style.display = "none"
+    gameDiv.style.display = "block"
+    player1header.innerHTML = this.usernameText.value
+    player2header.innerHTML = "IN ATTESA..."
+
+    player2move.style.opacity = "0%"
+    player2btn.style.opacity = "0%"
+
+
+
+    socket.emit("login",{username:  this.usernameText.value})
+    //player2container.innerHTML = ' <div class="inputGame"> <h3 style="color:coral;padding-bottom: 20px">In attesa di un giocatore... </h3> <select>'
+
+
+})
+
+this.randomBtn.addEventListener("click",()=>{
+    loginDiv.style.display = "none"
+    gameDiv.style.display = "block"
+    player1header.innerHTML = this.usernameText.value
+    player2header.innerHTML = "IN ATTESA..."
+
+    player2move.style.opacity = "0%"
+    player2btn.style.opacity = "0%"
+
+
+
+    socket.emit("login",{username:  this.usernameText.value})
+})
+
+
+socket.on("beginMatch",(message)=>{
+
+    if(message[0].nickname == this.usernameText.value){
+    player1header.innerHTML = message[0].nickname
+    player2header.innerHTML = message[1].nickname
+    }else{
+        player1header.innerHTML = message[1].nickname
+    player2header.innerHTML = message[0].nickname
+    }
+
+
+})
+
+
+array_mosse = {"CARTA" : 128196, "FORBICE" : 9988 , "SASSO":129521}
+
+player1btn.addEventListener("click",()=>{
+
+
+    this.miamossa = this.player1move.value;
+    this.numero_mosse_mie++;
+
+    socket.emit("action",{username: this.usernameText.value, move: this.miamossa })
+
+    div_move = document.createElement("div")
+    div_move.innreHTML = "MOSSA: &#"+array_mosse[this.miamossa]
+    div_move.classList = ["move_player1"]
+    this.currentdiv = div_move;
+
+    this.my_moves[0].innerHTML = this.my_moves[0].innerHTML +  '<div style="text-align:center" id="move_player1"> MOSSA:     &#'+ array_mosse[this.miamossa] + '</div>'
+    console.log(this.my_moves[0])
+
+   
+    el = document.getElementsByClassName("move_player2")
+    el[this.numero_mosse_sue].style.display= "block"
+  
+    if(this.mossacontro != null){
+        //nel server lo faccio pure per vedere chi ha vinto la partita
+        if((this.miamossa == "SASSO") && (this.mossacontro=="CARTA")){
+            vittorePlayer2++;
+            this.wp2.innerHTML = "WIN:  "+vittorePlayer2
+            sconfittePlayer1++;
+            this.lp1.innerHTML = "LOSE:  "+sconfittePlayer1
+            this.miamossa = null;
+            this.mossacontro = null;
+        }
+        else  if((this.miamossa == "SASSO") && (this.mossacontro=="CARTA")){
+            vittorePlayer2++;
+            this.wp2.innerHTML = "WIN:  "+vittorePlayer2
+            sconfittePlayer1++;
+            this.lp1.innerHTML = "LOSE:  "+sconfittePlayer1
+            this.miamossa = null;
+            this.mossacontro = null;
+        }else  if((this.miamossa == "SASSO") && (this.mossacontro=="FORBICE")){
+            vittorePlayer1++;
+            this.wp1.innerHTML = "WIN:  "+vittorePlayer1
+            sconfittePlayer2++;
+        this.lp2.innerHTML = "LOSE:  "+sconfittePlayer2
+            this.miamossa = null;
+            this.mossacontro = null;
+        }else if((this.miamossa == "CARTA") && (this.mossacontro=="FORBICE")){
+            vittorePlayer2++;
+            this.wp2.innerHTML = "WIN:  "+vittorePlayer2
+            sconfittePlayer1++;
+            this.lp1.innerHTML = "LOSE:  "+sconfittePlayer1
+            this.miamossa = null;
+            this.mossacontro = null;
+        }else  if((this.miamossa == "CARTA") && (this.mossacontro=="SASSO")){
+            vittorePlayer1++;
+            this.wp1.innerHTML = "WIN:  "+vittorePlayer1
+            sconfittePlayer2++;
+        this.lp2.innerHTML = "LOSE:  "+sconfittePlayer2
+            this.miamossa = null;
+            this.mossacontro = null;
+        }else if((this.miamossa == "FORBICE") && (this.mossacontro=="CARTA")){
+            vittorePlayer1++;
+            this.wp1.innerHTML = "WIN:  "+ vittorePlayer1
+            sconfittePlayer2++;
+        this.lp2.innerHTML = "LOSE:  "+sconfittePlayer2
+            this.miamossa = null;
+            this.mossacontro = null;
+        }else if((this.miamossa == "FORBICE") && (this.mossacontro=="SASSO")){
+            vittorePlayer2++;
+            this.wp2.innerHTML = "WIN:  "+vittorePlayer2
+            sconfittePlayer1++;
+            this.lp1.innerHTML = "LOSE:  "+sconfittePlayer1
+            this.miamossa = null;
+            this.mossacontro = null;
+        }else{
+            this.miamossa = null;
+            this.mossacontro = null;
+        }
+    }
+
+    
+})
+
+numero_mosse_mie = -1;
+numero_mosse_sue = -1;
+socket.on("againstMove",(message)=>{
+    
+    this.numero_mosse_sue++;
+    this.mossacontro = message.move;
+    console.log(this.numero_mosse_mie,this.numero_mosse_sue)
+    
+
+    div_move = document.createElement("div")
+    div_move.innerHTML = "MOSSA: "+message.move
+    div_move.classList = ["move_player2"]
+       
+    this.against_moves[0].innerHTML = this.against_moves[0].innerHTML +  '<div id="move_player2" class="move_player2"> MOSSA: &#'+array_mosse[message.move]+ '</div>'
+
+    if(this.miamossa == null && (this.numero_mosse_mie!=this.numero_mosse_sue)){
+        console.log(this.numero_mosse)
+        el = document.getElementsByClassName("move_player2")
+        el[this.numero_mosse_sue].style.display= "none"
+    }else{
+         el = document.getElementByClassName("move_player2")
+        el[this.numero_mosse_sue].style.display= "block"
+    }
+
+
+    //da qui mi calcolo se ho vinto oppure no
+
+    console.log(this.miamossa)
+    console.log(this.mossacontro)
+
+    if(this.miamossa != null){
+    //nel server lo faccio pure per vedere chi ha vinto la partita
+    if((this.miamossa == "SASSO") && (this.mossacontro=="CARTA")){
+        vittorePlayer2++;
+        this.wp2.innerHTML = "WIN:  "+vittorePlayer2
+        sconfittePlayer1++;
+            this.lp1.innerHTML = "LOSE:  "+sconfittePlayer1
+        this.miamossa = null;
+        this.mossacontro = null;
+    }
+    else  if((this.miamossa == "SASSO") && (this.mossacontro=="CARTA")){
+        vittorePlayer2++;
+        this.wp2.innerHTML = "WIN:  "+vittorePlayer2
+        sconfittePlayer1++;
+            this.lp1.innerHTML = "LOSE:  "+sconfittePlayer1
+        this.miamossa = null;
+        this.mossacontro = null;
+    }else  if((this.miamossa == "SASSO") && (this.mossacontro=="FORBICE")){
+        vittorePlayer1++;
+        this.wp1.innerHTML = "WIN:  "+vittorePlayer1
+        sconfittePlayer2++;
+        this.lp2.innerHTML = "LOSE:  "+sconfittePlayer2
+        this.miamossa = null;
+        this.mossacontro = null;
+    }else if((this.miamossa == "CARTA") && (this.mossacontro=="FORBICE")){
+        vittorePlayer2++;
+        this.wp2.innerHTML = "WIN:  "+vittorePlayer2
+        sconfittePlayer1++;
+            this.lp2.innerHTML = "LOSE:  "+sconfittePlayer1
+            sconfittePlayer2++;
+        this.lp2.innerHTML = "LOSE:  "+sconfittePlayer2
+        this.miamossa = null;
+            this.mossacontro = null;
+    }else  if((this.miamossa == "CARTA") && (this.mossacontro=="SASSO")){
+        vittorePlayer1++;
+        this.wp1.innerHTML = "WIN:  "+vittorePlayer1
+        sconfittePlayer2++;
+        this.lp2.innerHTML = "LOSE:  "+sconfittePlayer2
+        this.miamossa = null;
+            this.mossacontro = null;
+    }else if((this.miamossa == "FORBICE") && (this.mossacontro=="CARTA")){
+        vittorePlayer1++;
+        this.wp1.innerHTML = "WIN:  "+vittorePlayer1
+        sconfittePlayer2++;
+        this.lp2.innerHTML = "LOSE:  "+sconfittePlayer2
+        this.miamossa = null;
+            this.mossacontro = null;
+    }else if((this.miamossa == "FORBICE") && (this.mossacontro=="SASSO")){
+        vittorePlayer2++;
+        this.wp2.innerHTML = "WIN:  "+vittorePlayer2
+        sconfittePlayer1++;
+            this.lp1.innerHTML = "LOSE:  "+sconfittePlayer1
+        this.miamossa = null;
+            this.mossacontro = null;
+    }else{
+        this.miamossa = null;
+        this.mossacontro = null;
+    }
+}
+
+})
+
+
+
+socket.on("win_partita",()=>{
+    alert("HAI VINTO");
+})
+
+
+socket.on("lose_partita",()=>{
+    alert("HAI PERSO");
+})
